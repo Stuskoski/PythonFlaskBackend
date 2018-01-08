eb781523757e1089368de53dcf91c544305db88d
StuskoskiHEB <rutkoski.augustus@heb.com>
2018-01-08 11:38:58 -0600

    added get all call

--- application.py
+++ application.py
@@ -1,10 +1,15 @@
 #!flask/bin/python
-from flask import Flask
+from flask import Flask, jsonify
 from flaskrun import flaskrun
 from flask_cors import CORS
+import boto3
 
 application = Flask(__name__)
 CORS(application)
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+table = dynamodb.Table('Products')
+
 
 @application.route('/', methods=['GET'])
 def get():
@@ -14,6 +19,12 @@ def get():
 @application.route('/', methods=['POST'])
 def post():
     return '{"Output":"Hello World"}'
+
+
+@application.route('/getAllProducts', methods=['GET'])
+def get_all_products():
+    response = table.scan()
+    return jsonify(response['Items'])
 
 
 if __name__ == '__main__':
