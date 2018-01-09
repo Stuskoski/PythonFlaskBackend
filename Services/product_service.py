16b587963ac29793094f6eb4bf8c5b6c7b1473aa
StuskoskiHEB <rutkoski.augustus@heb.com>
2018-01-09 15:55:52 -0600

    added more product service calls for python backend

--- Services/product_service.py
+++ Services/product_service.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, json
-from Util.default_serializers import default
+from flask import Blueprint, json, request
+from Util.default_serializers import DecimalEncoder
 import boto3
+from boto3.dynamodb.conditions import Key, Attr
 
 productservice = Blueprint('productservice', __name__)
 dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
@@ -10,5 +11,36 @@ table = dynamodb.Table('Products')
 @productservice.route('/getAllProducts', methods=['GET'])
 def get_all_products():
     response = table.scan()
-    return json.dumps(response['Items'], default=default)
+    return json.dumps(response['Items'], cls=DecimalEncoder)
+
+
+@productservice.route('/getProductInformation', methods=['POST'])
+def get_product_info_with_id():
+
+    product_id = request.get_json()['productID']
+    response = table.query(
+        ProjectionExpression="#Desc, HAS_OFFERS, ID, IMAGE_URL",
+        ExpressionAttributeNames={"#Desc": "DETAIL"},
+        KeyConditionExpression=Key('ID').eq(product_id)
+    )
+
+    return json.dumps(response['Items'], cls=DecimalEncoder)
+
+
+@productservice.route('/getProductsWithOffers', methods=['GET'])
+def get_products_with_offers():
+    response = table.scan()
+
+    has_offer_items = list(filter(lambda items: items['HAS_OFFERS'], response['Items']))
+
+    return json.dumps(has_offer_items, cls=DecimalEncoder)
+
+
+@productservice.route('/getProductsWithoutOffers', methods=['GET'])
+def get_products_without_offers():
+    response = table.scan()
+
+    has_offer_items = list(filter(lambda items: not items['HAS_OFFERS'], response['Items']))
+
+    return json.dumps(has_offer_items, cls=DecimalEncoder)
 
--- Util/default_serializers.py
+++ Util/default_serializers.py
@@ -1,7 +1,19 @@
 from decimal import Decimal
+import json
+import decimal
 
 
 def default(obj):
     if isinstance(obj, Decimal):
         return str(obj)
     raise TypeError("Object of type '%s' is not JSON serializable" % type(obj).__name__)
+
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
